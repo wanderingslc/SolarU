e6331deb826bd521c38d1e4ca16e1c97264ad0ca
Derek <derekweatherford@gmail.com>
2014-01-23 19:14:23 -0700

    add instance variables to the index action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
+  
+  before_filter :check_data, :only => [:index]
+
   def index
-  end
+    @energyLifetimeData = EnergyLifetimeArray.last.parsed_array
+    @energyMonthlyData = SolarData.retrieve_monthly_data
+    @energyWeeklyData = SolarData.retrieve_weekly_data
+    @totalOutput = EnergyLifetimeArray.last.raw_array.reduce(:+)
+    @averageOutput = (@totalOutput / (EnergyLifetimeArray.last.raw_array.count))
+    @highestOutput = EnergyLifetimeArray.last.raw_array.max
+   end
+ 
+ 
+private
+  def check_data # if database is empty, get data
+    SolarData.get_energy_lifetime if EnergyLifetimeArray.last.nil? 
+    SolarData.get_monthly_production if MonthlyData.last.nil?
+    SolarData.get_weekly_production if WeeklyData.last.nil?
+    SolarData.get_current_production if DailyProduction.last.nil?
+  end 
+end
+
 end
