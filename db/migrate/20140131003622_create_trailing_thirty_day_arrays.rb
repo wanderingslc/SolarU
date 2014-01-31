c5d15707d6754970b90e022be17f3ecee55c5d67
Derek <derekweatherford@gmail.com>
2014-01-31 15:40:24 -0700

    remove improperly named model, replace it with last_seven_day_array.rb. Add functioning solar_data call to get production data for the last 7 days.

--- db/migrate/20140131003622_create_trailing_thirty_day_arrays.rb
+++
@@ -1,9 +0,0 @@
-class CreateTrailingThirtyDayArrays < ActiveRecord::Migration
-  def change
-    create_table :trailing_thirty_day_arrays do |t|
-      t.text :power_array
-      t.integer :start_date
-      t.timestamps
-    end
-  end
-end
