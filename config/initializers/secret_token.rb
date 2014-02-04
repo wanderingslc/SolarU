b20976418b3e8933f75615cabccea14123bc9a3b
Derek <derekweatherford@gmail.com>
2014-02-03 17:49:26 -0700

    Blah

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SolarU::Application.config.secret_token = '48e9b29b99fb8f692d3f3de55e076dbe0b204893ef5814f66749aba76aaf6ce07e5223b0719ba52a892cb8859ef953b24f2786a0e9b6bc69d4cdf09c911adb23'
+SolarU::Application.config.secret_token = ENV['SCRET_TOKEN']
