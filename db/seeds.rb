b193e96b004cf631edf788ab4986585eb8679555
Quintin Adam <QuintinAdam@live.com>
2014-02-06 17:36:24 -0700

    adds a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# Environment variables (ENV['...']) can be set in the file config/application.yml.
-# See http://railsapps.github.io/rails-environment-variables.html
+puts "Populating the database\n"
+puts "Getting Current production"
+SolarData.get_current_production
+puts "Getting lifetime Energy"
+SolarData.get_energy_lifetime
+puts "Current Month Data"
+SolarData.split_current_data_into_months
+puts "Getting the last 7 days"
+SolarData.get_trailing_seven_days
+puts "Getting the weekly data"
+SolarData.get_weekly_production
+puts "Getting the weather data..."
+puts "This might take a while..."
+WeatherData.get_weather_data
